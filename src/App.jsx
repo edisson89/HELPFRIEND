823851d5cdbf01cca50315746f32bf52be2d2820
edisson89 <edisson.12@hotmail.com>
2024-07-10 11:34:22 -0500

    agregando redes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,45 @@
+import { useRef } from 'react';
 import './App.css'
 import image from './assets/BinancePayQR.png'
+import { useState } from 'react';
 
 function App(){
+  const url = encodeURIComponent(window.location.href);
+  const text = encodeURIComponent('¡Estamos aquí para ayudar.Ayudame a compartir esta increíble página web y mejora una Vida!');
+  
+  const socialMediaLinks = {
+    facebook: `https://www.facebook.com/sharer/sharer.php?u=${url}`,
+    twitter: `https://twitter.com/intent/tweet?url=${url}&text=${text}`,
+    linkedin: `https://www.linkedin.com/sharing/share-offsite/?url=${url}`,
+    whatsapp: `https://wa.me/?text=${text} ${url}`
+  };
+
+  const textRef = useRef(null);
+  const [message, setMessage] = useState('');
+
+  const text2Ref = useRef(null);
+  const [message2, setMessage2] = useState('');
+  const copiarTexto = async () => {
+    try {
+      await navigator.clipboard.writeText(textRef.current.value);
+      setMessage('Texto copiado al portapapeles');
+    } catch (err) {
+      setMessage('Fallo al copiar el texto');
+    }
+  };
+  const copiar2Texto = async () => {
+    try {
+      await navigator.clipboard.writeText(text2Ref.current.value);
+      setMessage2('Texto copiado al portapapeles');
+    } catch (err) {
+      setMessage2('Fallo al copiar el texto');
+    }
+  };
   return(     
     
     <div>
       <header>
-      <h1>Ayudame a ayudar a un amigo</h1>
+      <h1>Ayudame a ayudar la salud de un amigo</h1>
       <p>
       Petición de Colaboración
 
@@ -29,16 +62,49 @@ Con mucha estima y solidaridad espero que cada granito de arena aporte una valio
       </p>
       </header>
       <main>
-        <h2>Billetera:Tron(TRC20) TTRBNvnJKVEoZbPj5XH7mEUPyTzpQ2oQoU</h2>
-        <h2>Billetera:(BTC) 12Yxen26xPFZVYUWZ647uVRn5eBYTCt9St</h2>
-        <div style={{display:'flex',wrap:' nowrap',justifyContent: 'center'}}>
+        <h2>Billetera USDT:Tron(TRC20) 
+          <textarea style={{padding:5,marginLeft:10,borderRadius:10,width:'280px'}} ref={textRef} defaultValue="TTRBNvnJKVEoZbPj5XH7mEUPyTzpQ2oQoU" readOnly>
+          </textarea>
+          </h2>
+          <button onClick={copiarTexto} style={{ marginTop: '1px', padding: '10px 20px', fontSize: '1em', cursor: 'pointer' }}>
+        Copiar billetera
+      </button>
+      {message && <p>{message}</p>}
 
-        <img style={{display:'flex',justifyContent:'center',alignItems:'center', backgroundColor:'gray'}} src={image} alt='codigo de ayuda' height='auto' width='200'></img>
+        <h2>  Billetera BTC:(BTC) 
+        <textarea style={{padding:5,marginLeft:10,borderRadius:10,width:'280px'}} ref={text2Ref} defaultValue="12Yxen26xPFZVYUWZ647uVRn5eBYTCt9St" readOnly>
+        </textarea>
+        </h2>
+        <button onClick={copiar2Texto} style={{ marginTop: '1px', padding: '10px 20px', fontSize: '1em', cursor: 'pointer' }}>
+        Copiar billetera
+      </button>
+      {message2 && <p>{message2}</p>}
+    
+        <div style={{display:'flex',wrap:' nowrap',justifyContent: 'center'}}>
+   
+  <br />
+        <img style={{display:'flex',justifyContent:'center',alignItems:'center', backgroundColor:'gray'}} src={image} alt='codigo de ayuda' height='auto' width='220'></img>
         </div>
         
 
       </main>
-      <footer></footer>
+      <footer>
+      <h1>Compartir esta Página</h1>
+      <div  style={{display:'flex',margin:10,justifyContent:'center',alignItems:'center'}}   className="social-buttons">
+        <a href={socialMediaLinks.facebook} target="_blank" rel="noopener noreferrer">
+          <button>Compartir en Facebook</button>
+        </a>
+        <a href={socialMediaLinks.twitter} target="_blank" rel="noopener noreferrer">
+          <button>Compartir en Twitter</button>
+        </a>
+        <a href={socialMediaLinks.linkedin} target="_blank" rel="noopener noreferrer">
+          <button>Compartir en LinkedIn</button>
+        </a>
+        <a href={socialMediaLinks.whatsapp} target="_blank" rel="noopener noreferrer">
+          <button>Compartir en WhatsApp</button>
+        </a>
+      </div>
+      </footer>
     </div>
     
   )
